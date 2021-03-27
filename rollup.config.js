363a3bef46fad49ce8207b96de4d8ec9128778c6
Wael wahbeh <waelio@users.noreply.github.com>
2021-03-28 01:53:16 +0300

    adding resolvers

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,31 @@
-export default [{
-  input: 'src/store.js',
-  output: {
-    file: 'dist/store.js',
-    format: 'cjs'
-  }
-}, {
-  input: 'src/utils.js',
-  output: [
-    {
-      file: 'dist/waelioUtils.js',
+import resolve from '@rollup/plugin-node-resolve';
+
+export default 
+  [{
+    input: 'src/store.js',
+    plugins: [ resolve() ],
+    output: {
+      file: 'dist/store.js',
       format: 'cjs'
-    },
-    {
-      file: 'dist/waelioUtils-umd.js',
-      name: 'waelioUtils',
-      format: 'umd'
-    },
-    {
-      file: 'dist/waelioUtils-es.js',
-      format: 'es'
     }
+  },
+  {
+    input: 'src/utils.js',
+    plugins: [ resolve() ],
+    output: [
+      {
+        file: 'dist/waelioUtils.js',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/waelioUtils-umd.js',
+        name: 'waelioUtils',
+        format: 'umd'
+      },
+      {
+        file: 'dist/waelioUtils-es.js',
+        format: 'es'
+      }
+    ]
+  }  
   ]
-}];
