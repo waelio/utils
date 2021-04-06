00ed1a13c2c4f43ec5efdd8365de979be9e8f68f
Wael wahbeh <waelio@users.noreply.github.com>
2021-04-06 08:02:31 +0300

    more outputs

--- rollup.config.js
+++ rollup.config.js
@@ -6,20 +6,44 @@ export default [
   {
     input: 'src/plugins',
     plugins: [resolve(), json(), commonjs()],
-    output: {
-      name: 'plugins',
-      file: 'dist/plugins.js',
-      format: 'es'
-    }
+    output: [
+      {
+        name: 'plugins',
+        file: 'dist/plugins.ejs',
+        format: 'es'
+      },
+      {
+        name: 'plugins',
+        file: 'dist/plugins.js',
+        format: 'umd'
+      },
+      {
+        name: 'plugins',
+        file: 'dist/plugins.cjs',
+        format: 'cjs'
+      }
+    ]
   },
   {
     input: 'src/plugins/config',
     plugins: [resolve(), json(), commonjs()],
-    output: {
-      name: 'config',
-      file: 'dist/config.js',
-      format: 'cjs'
-    }
+    output: [
+      {
+        name: 'config',
+        file: 'dist/config.ejs',
+        format: 'es'
+      },
+      {
+        name: 'config',
+        file: 'dist/config.js',
+        format: 'umd'
+      },
+      {
+        name: 'config',
+        file: 'dist/config.cjs',
+        format: 'cjs'
+      }
+    ]
   },
   {
     input: 'src/plugins/note',
@@ -27,12 +51,17 @@ export default [
     output: [
       {
         name: 'note',
-        file: 'dist/note.js',
+        file: 'dist/note.ejs',
         format: 'es'
       },
       {
         name: 'note',
         file: 'dist/note.js',
+        format: 'umd'
+      },
+      {
+        name: 'note',
+        file: 'dist/note.cjs',
         format: 'cjs'
       }
     ]
@@ -43,17 +72,17 @@ export default [
     output: [
       {
         name: 'store',
-        file: 'dist/store.js',
+        file: 'dist/store.ejs',
         format: 'es'
       },
       {
         name: 'store',
-        file: 'dist/store-umd.js',
+        file: 'dist/store.js',
         format: 'umd'
       },
       {
         name: 'store',
-        file: 'dist/store-cjs.js',
+        file: 'dist/store.cjs',
         format: 'cjs'
       }
     ]
@@ -63,18 +92,18 @@ export default [
     plugins: [resolve(), json(), commonjs()],
     output: [
       {
-        file: 'dist/waelioUtils.js',
+        file: 'dist/waelioUtils.cjs',
         format: 'cjs',
         name: 'waelioUtils'
       },
       {
-        file: 'dist/waelioUtils-umd.js',
+        file: 'dist/waelioUtils.js',
         name: 'waelioUtils',
         format: 'umd',
         name: 'waelioUtils'
       },
       {
-        file: 'dist/waelioUtils-es.js',
+        file: 'dist/waelioUtils.ejs',
         format: 'es',
         name: 'waelioUtils'
       }
