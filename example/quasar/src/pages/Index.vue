498fcf99483f5fc494b1fd65b20602e23d8e71ba
Wael wahbeh <waelio@users.noreply.github.com>
2021-05-03 09:44:54 +0300

    pre push

--- example/quasar/src/pages/Index.vue
+++ example/quasar/src/pages/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
   </q-page>
 </template>
-
 <script>
+// import * as note from "@waelio/utils/dist/utils";
+
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  mounted() {
+    // this.conf = config.get("client");
+    // note.success("mounted");
+  },
+  data() {
+    return {
+      conf: null
+    };
+  }
+};
 </script>
